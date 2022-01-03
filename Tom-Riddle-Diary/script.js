27c19b4afba62d65e1c3999078a8cde2b1daaf7a
outshaker <wind.sixwings@gmail.com>
2022-01-04 07:07:35 +0800

    feat: replay, load, save

--- Tom-Riddle-Diary/script.js
+++ Tom-Riddle-Diary/script.js
@@ -1,7 +1,9 @@
+const sleep = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds))
+
 const getTimer = () => {
   const start = Date.now()
   return () => {
-    return Math.floor((Date.now() - start) / 1000)
+    return Date.now() - start
   }
 }
 
@@ -42,4 +44,41 @@ window.addEventListener('load', () => {
     bar.focus()
   })
 })
+
+const play = () => {
+  const list = [...document.querySelectorAll('.logger>div')].map((node) => ({ t: parseInt(node.getAttribute('t')), text: node.innerText }))
+  if (!list) {
+    console.warn('logger has no record')
+  }
+
+  const show = async (list, i) => {
+    const loader = document.querySelector('.loader')
+    const node = document.createElement('div')
+    node.innerText = list[i].text
+    node.addEventListener('animationend', () => {
+      node.remove()
+    })
+    loader.appendChild(node)
+    if (i+1 < list.length) {
+      let wait_t = list[i+1].t - list[i].t
+      wait_t = (wait_t > 30 * 1000) ? 30 * 1000 : wait_t // max 30s
+      console.log(`wait ${wait_t}ms for No.${i+1}`)
+      await sleep(wait_t)
+      show(list, i+1)
+    }
+  }
+  show(list, 0)
+}
+
+const save = () => {
+  const logger = document.querySelector('.logger')
+  console.log(logger.innerHTML)
+  window.localStorage.setItem('log', logger.innerHTML)
+}
+
+const load = () => {
+  const data = window.localStorage.getItem('log') || ''
+  const logger = document.querySelector('.logger')
+  logger.innerHTML = data
+}
 
